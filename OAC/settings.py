628bf7a8d50210d1d18e9f0f671289931aab5a68
Mzomba <dubemzomba@gmail.com>
2025-05-27 13:52:01 +0200

    changes

--- OAC/settings.py
+++ OAC/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'Sizolwethu',
         'HOST':'127.0.0.1',
-        'PORT':'3306',
+        'PORT':'3307',
         'OPTIONS': {
             'charset': 'utf8mb4'
         }
